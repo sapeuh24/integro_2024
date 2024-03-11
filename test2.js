ffd0e2537b613cc1bc4d0110970d1aced39ceab8
amilkar <poltergeist84@hotmail.com>
2024-03-11 01:10:41 -0500

    prueba integro 2024 completada

--- test2.js
+++ test2.js
@@ -1,7 +1,20 @@
-var input = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17]
+var input = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17]
 
 function apples(input) {
+    const applesInBoxes = 4;
+    const boxesInPackage = 2;
+    const finalPackages = [];
 
+    while (input.length > 0) {
+        const boxes = [];
+        for (let i = 0; i < boxesInPackage; i++) {
+            const box = input.splice(0, applesInBoxes);
+            boxes.push(box);
+        }
+        finalPackages.push(boxes);
+    }
+
+    return finalPackages;
 }
 
 console.log(apples(input))
--- test4.jsx
+++ test4.jsx
@@ -1,12 +1,50 @@
-import React from 'react';
-import axios from 'axios';
+import React, { useState } from "react";
+import axios from "axios";
 
 export default class Palindrome extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      palindrome: "",
+      result: "",
+    };
+  }
 
-    render() {
-        return (
-            <>
-            </>
-        )
-    }
+  handleChange = (e) => {
+    this.setState({ palindrome: e.target.value });
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    const data = {
+      palindrome: this.state.palindrome,
+    };
+    axios
+      .post("http://localhost:3000/palindrome", data)
+      .then((res) => {
+        this.setState({ result: res.data });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            Palindrome:
+            <input
+              type="text"
+              value={this.state.palindrome}
+              onChange={this.handleChange}
+            />
+          </label>
+          <button type="submit">Check</button>
+        </form>
+        <p>{this.state.result}</p>
+      </div>
+    );
+  }
 }
